17c2984a0ddf876cfe3d5f6e4eb2671f71a39229
jp-quintana <juanquintana1996@gmail.com>
2024-02-05 01:18:57 -0300

    wip5: desafio clase 20

--- desafio-clase-19/src/routes/views.routes.js
+++ desafio-clase-19/src/routes/views.routes.js
@@ -14,5 +14,6 @@ router.get('/products', renderProductsPage);
 router.get('/product/:pid', renderProductPage);
 router.get('/cart/:cid', renderCartPage);
 router.get('/chat', renderChatPage);
+// router.get('/login', renderLoginPage);
 
 export default router;
